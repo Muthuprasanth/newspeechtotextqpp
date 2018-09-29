540c6f413940fd6aebcb5a8f8c26628739710b48
mprasanth113@gmail.com <mprasanth113@gmail.com>
2018-09-29 21:25:00 +0530

    02 commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var fs = require("fs");
+var request = require('request');
+var url = require('url');
+var request = require('request-promise').defaults({ encoding: null });
+var toWav = require('audiobuffer-to-wav')
+//var audiobuffer = require('audio-buffer')
+const AudioContext = require('web-audio-api').AudioContext;
+const audioContext = new AudioContext;
+const { BingSpeechClient, VoiceRecognitionResponse } = require('bingspeech-api-client');
+
+function getfile(url,token)
+{
+  return request({
+    url: url,
+    headers: {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/octet-stream'
+    }
+  });
+}
+
+router.get('/speechtotext', function(req, res, next) {
+  res.send("thanks wor working");
 });
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
